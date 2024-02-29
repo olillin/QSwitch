66b28f03ba7790403791ba0367578ce87055d3b5
Oli <oli@olillin.com>
2024-02-29 13:25:56 +0100

    Fix not being able to switch to a template without parameters

--- background.js
+++ background.js
@@ -34,14 +34,20 @@ function urlFromTemplate(url, pattern, template) {
     const templateMatches = Array.from(template.matchAll(/(?<!\\)\$(\d+)/g))
     console.log(parameters)
     let newUrl = ''
-    let i = 0
-    templateMatches.forEach(match => {
-        let parameterIndex = parseInt(match[1])
-        template.substr(i, match.index) + parameters[parameterIndex]
-        newUrl += template.substr(i, match.index-i) + parameters[parameterIndex]
-        i = match.index + match[0].length
-    })
+    if (templateMatches.length > 0) {
+        let i = 0
+        templateMatches.forEach(match => {
+            let parameterIndex = parseInt(match[1])
+            template.substr(i, match.index) + parameters[parameterIndex]
+            newUrl += template.substr(i, match.index - i) + parameters[parameterIndex]
+            i = match.index + match[0].length
+        })
+    } else {
+        newUrl = template
+    }
+    // Remove backslash from escaped template parameters
     newUrl = newUrl.replace(/\\(?=\$\d+)/g, '')
+
     return newUrl
 }
 
